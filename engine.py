9e4477f13506024f9099374f217cff2fa3d459c8
Тимофей Золотов <timofejzolotov@192.168.1.11>
2024-01-17 21:52:30 +0300

    Change Engine class

--- engine.py
+++ engine.py
@@ -2,28 +2,16 @@ import chess
 
 MAX_DEPTH = 3
 
+MOVE_WEIGHT = 2
 piece_values = {
-    chess.PAWN: 10,
-    chess.ROOK: 50,
-    chess.KNIGHT: 32,
-    chess.BISHOP: 33,
-    chess.QUEEN: 90,
-    chess.KING: 40
+    None: 0,
+    chess.PAWN: 100,
+    chess.ROOK: 500,
+    chess.KNIGHT: 320,
+    chess.BISHOP: 330,
+    chess.QUEEN: 900,
+    chess.KING: 400
 }
-
-
-def calculate_advantage(board):
-    advantage = len(list(board.legal_moves))
-    if not advantage:
-        return float('-inf')
-    for square, piece in board.piece_map().items():
-        piece_value = piece_values[piece.piece_type]
-        if piece.color == board.turn:
-            advantage += piece_value
-        else:
-            advantage -= piece_value
-
-    return advantage
 
 
 class Engine:
@@ -34,12 +22,36 @@ class Engine:
     def __call__(self):
         print('-' * 211)
         advantage, move = self.find_best_move(MAX_DEPTH)
+        if self.board.piece_at(move.to_square):
+            self.advantage += piece_values[self.board.piece_at(move.to_square).piece_type]
         self.board.push(move)
+
+        print(f'Engine advantage: {self.advantage}')
+
+    def push(self, move):
+        if self.board.piece_at(move.to_square):
+            self.advantage -= piece_values[self.board.piece_at(move.to_square).piece_type]
+        self.board.push(move)
+
+        print(f'Engine advantage: {self.advantage}')
+
+    def calculate_advantage(self):
+        advantage = len(list(self.board.legal_moves)) * MOVE_WEIGHT
+        if not advantage:
+            return float('-inf')
+        for square, piece in self.board.piece_map().items():
+            piece_value = piece_values[piece.piece_type]
+            if piece.color == self.board.turn:
+                advantage += piece_value
+            else:
+                advantage -= piece_value
+
+        return advantage
 
     def find_best_move(self, depth):
         legal_moves = list(self.board.legal_moves)
         if depth == 0 or not legal_moves:
-            return calculate_advantage(self.board), None
+            return self.calculate_advantage(), None
         else:
             best_move = None
             max_advantage = float('-inf')
@@ -54,9 +66,8 @@ class Engine:
                     best_move = move
 
                 if depth == MAX_DEPTH:
-                    print(move, calculate_advantage(self.board), advantage)
+                    print(move, self.calculate_advantage(), advantage)
 
                 self.board.pop()
 
             return max_advantage, best_move
-
